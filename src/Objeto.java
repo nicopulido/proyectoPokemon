d5646753d562817f93c28f74f37bb0bafea75294
nicopulido <nicopipe2010@gmail.com>
2024-04-17 10:22:06 -0500

    Agregué sus atributos y el constructor

--- src/Objeto.java
+++ src/Objeto.java
@@ -1,12 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author ElPul
- */
-public class Objetos {
+public class Objeto {
+
+    private String nombre;
+    private String descripcion;
     
+    //constructor para crear el objeto
+    public Objeto(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    //getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
 }
