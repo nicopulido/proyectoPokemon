79534d5510ab1ba332a847ca621ddeaa5833f5e1
estudiantes <estudiantes@10.20.170.35>
2024-04-17 12:47:55 -0500

    Crear constructor, getter y setters y agregar unos cuantos metodos

--- src/Caja.java
+++ src/Caja.java
@@ -9,4 +9,58 @@
  */
 public class Caja {
     
+	//agregar los atributos
+	
+	private String fondo;
+	private int num_Espacios;
+	private int num_Caja;
+	private boolean espacio_Ocupado;
+	
+	//constructores
+	public Caja () {}
+	
+	public Caja (String fondo, int num_Espacios, int num_Caja, boolean espacio_Ocupado) {
+		this.espacio_Ocupado= espacio_Ocupado;
+		this.fondo= fondo;
+		this.num_Caja= num_Caja;
+		this.num_Espacios= num_Espacios;
+	}
+
+	//Getter and Setter
+	public String getFondo() {
+		return fondo;
+	}
+
+	public void setFondo(String fondo) {
+		this.fondo = fondo;
+	}
+
+	public int getNum_Espacios() {
+		return num_Espacios;
+	}
+
+	public void setNum_Espacios(int num_Espacios) {
+		this.num_Espacios = num_Espacios;
+	}
+
+	public int getNum_Caja() {
+		return num_Caja;
+	}
+
+	public void setNum_Caja(int num_Caja) {
+		this.num_Caja = num_Caja;
+	}
+
+	public boolean isEspacio_Ocupado() {
+		return espacio_Ocupado;
+	}
+
+	public void setEspacio_Ocupado(boolean espacio_Ocupado) {
+		this.espacio_Ocupado = espacio_Ocupado;
+	}
+	
+	//metodos
+	
+	public void Mostrar_Pokemon() {}
+	public void Mover_Pokemon() {}
 }
