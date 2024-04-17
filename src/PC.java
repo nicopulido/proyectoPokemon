8e7f7c4af03e10702a29a5f095d968a6f07b3098
nicopulido <nicopipe2010@gmail.com>
2024-04-17 15:35:16 -0500

    Añadí algunos métodos y atributos

--- src/PC.java
+++ src/PC.java
@@ -10,31 +10,41 @@ import java.util.ArrayList;
  * @author ElPul
  */
 public class PC {
-    
-	private ArrayList<Caja> cajas;
-	private ArrayList<Objeto> objetos;
-	//constructores
-	
-	public PC() {}
-	public PC(ArrayList<Caja> cajas, ArrayList<Objeto> objetos) {
-		this.cajas= cajas;
-		this.objetos= objetos;
-	}
-	
-	//Getter and Setter
-	public ArrayList<Caja> getCajas() {
-		return cajas;
-	}
-	public void setCajas(ArrayList<Caja> cajas) {
-		this.cajas = cajas;
-	}
-	
-	//netodos
-	
-	public void almacenar_Pokemon() {}
-	public void eliminar_POkemon() {}
-	public void almacenar_Objeto() {}
-	public void eliminar_Objeto() {}
 
-	
+    private ArrayList<Caja> cajas;
+    private ArrayList<Objeto> objetos;
+    private Pokemon pokemonSeleccionado;
+    //constructores
+
+    public PC() {
+    }
+
+    public PC(ArrayList<Caja> cajas, ArrayList<Objeto> objetos) {
+        this.cajas = cajas;
+        this.objetos = objetos;
+    }
+
+    //Getter and Setter
+    public ArrayList<Caja> getCajas() {
+        return cajas;
+    }
+
+    public void setCajas(ArrayList<Caja> cajas) {
+        this.cajas = cajas;
+    }
+
+    //netodos
+    public void almacenar_Pokemon() {
+    }
+
+    public void eliminar_Pokemon() {
+    }
+
+    public void almacenar_Objeto() {
+    }
+
+    public void eliminar_Objeto() {
+    }
+    
+    
 }
